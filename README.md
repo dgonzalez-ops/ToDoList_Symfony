a9f7ade928868a1b594696e96fe8358aac7d8805
dgonzalez-ops <diegogonzalezalcazar@gmail.com>
2025-03-17 22:06:11 +0100

    Add basic Tarea entity, repository, and CRUD structure
    Introduced the Tarea entity with a corresponding repository and initial CRUD routes in the controller. Updated templates for listing, creating, and editing tasks, and integrated the menu layout in the base template for better navigation. Added a database migration to support the Tarea table and updated documentation in README.md.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 
 ### Create DataBase
 - php bin/console doctrine:database:create
+
+### Create Entity
+- php bin/console make:entity
+
+### Create Migration (Copy of the Database code)
+- php bin/console make:migration
+- php bin/console doctrine:migrations:migrate
+
--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -2,16 +2,42 @@
 
 namespace App\Controller;
 
+use App\Repository\TareaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TareaController extends AbstractController
 {
-    #[Route('/tarea', name: 'app_tarea')]
-    public function index(): Response
+    #[Route('/', name: 'app_listado_tarea')]
+    public function listado(TareaRepository $tareaRepository): Response
     {
-        return $this->render('tarea/index.html.twig', [
+        $tareas = $tareaRepository->findAll();
+        return $this->render('tarea/listado.html.twig', [
+            'tareas' => $tareas,
+        ]);
+    }
+
+    #[Route('/tarea/crear', name: 'app_crear_tarea')]
+    public function crear(): Response
+    {
+        return $this->render('tarea/crear.html.twig', [
+            'controller_name' => 'TareaController',
+        ]);
+    }
+
+    #[Route('/tarea/editar/{id}', name: 'app_editar_tarea')]
+    public function editar(int $id): Response
+    {
+        return $this->render('tarea/editar.html.twig', [
+            'controller_name' => 'TareaController',
+        ]);
+    }
+
+    #[Route('/tarea/eliminar/{id}', name: 'app_eliminar_tarea')]
+    public function eliminar(int $id): Response
+    {
+        return $this->render('tarea/editar.html.twig', [
             'controller_name' => 'TareaController',
         ]);
     }
